d53559ec10ea7ead1a44096abfda2ddad4ecb1fb
szymonorz <originalemail001@gmail.com>
2022-05-08 00:54:52 +0200

    Backend: Fix typo in old_password.

--- movietown/api/common.go
+++ movietown/api/common.go
@@ -1,6 +1,6 @@
 package api
 
 type changePasswordBody struct {
-	OldPassword string `json:"old_passowrd" validate:"required"`
+	OldPassword string `json:"old_password" validate:"required"`
 	NewPassword string `json:"new_password" validate:"required"`
 }
