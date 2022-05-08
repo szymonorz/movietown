5f24ed0e4b8d7f63bfa1b0a5d286519bf75b959d
szymonorz <originalemail001@gmail.com>
2022-05-08 23:13:19 +0200

    SQL: More dummy data.

--- sql/03_data.sql
+++ sql/03_data.sql
@@ -13,14 +13,63 @@ INSERT INTO discount_types(type, discount) VALUES('senior', 30);
 INSERT INTO movie_halls(name, number_of_seats) VALUES('DN',100);
 INSERT INTO movie_halls(name, number_of_seats) VALUES('DZ',60);
 
-INSERT INTO movies(title, director, description, length, url) VALUES('AAAAAAA','AAAA','aa',120, 'http://localhost:4000/image/painandsuffering.png');
-INSERT INTO movies(title, director, description, length, url) VALUES('BBBBBBBB','AAAA','ba',120, 'http://localhost:4000/image/painandsuffering.png');
+INSERT INTO movies(title, director, description, length, url) VALUES('Pain and suffering','Jane Doe','A story about making this horrible project my god i hate it.',120, 'http://localhost:4000/image/painandsuffering.png');
+INSERT INTO movies(title, director, description, length, url) VALUES('Spiderman: Far From Home','Jon Watts','Following the events of Avengers: Endgame (2019), Spider-Man must step up to take on new threats in a world that has changed forever.',120, 'http://localhost:4000/image/painandsuffering.png');
+INSERT INTO movies(title, director, description, length, url) VALUES('Doctor Strange in the Multiverse of Madness','Sam Raimi','Dr. Stephen Strange casts a forbidden spell that opens the doorway to the multiverse, including alternate versions of himself, whose threat to humanity is too great for the combined forces of Strange, Wong, and Wanda Maximoff.',126, 'http://localhost:4000/image/painandsuffering.png');
+INSERT INTO movies(title, director, description, length, url) VALUES('The Northman','Robert Eggers','From visionary director Robert Eggers comes The Northman, an action-filled epic that follows a young Viking prince on his quest to avenge his father''s murder',137, 'http://localhost:4000/image/painandsuffering.png');
+
 
 INSERT INTO movie_movie_types(movie_id, movie_type_id) VALUES(1,1);
 INSERT INTO movie_movie_types(movie_id, movie_type_id) VALUES(1,2);
 INSERT INTO movie_movie_types(movie_id, movie_type_id) VALUES(2,1);
 INSERT INTO movie_movie_types(movie_id, movie_type_id) VALUES(2,2);
+INSERT INTO movie_movie_types(movie_id, movie_type_id) VALUES(3,1);
+INSERT INTO movie_movie_types(movie_id, movie_type_id) VALUES(3,2);
+INSERT INTO movie_movie_types(movie_id, movie_type_id) VALUES(4,1);
+INSERT INTO movie_movie_types(movie_id, movie_type_id) VALUES(4,2);
 
 INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
 VALUES (1,1,'2022-04-25T15:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (1,1,'2022-05-09T15:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (1,1,'2022-05-09T08:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (1,1,'2022-05-10T10:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (1,1,'2022-05-10T15:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (1,1,'2022-05-11T10:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (1,1,'2022-05-12T15:30:00');
+
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (3,1,'2022-04-25T15:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (3,1,'2022-05-09T15:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (3,1,'2022-05-09T08:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (3,1,'2022-05-10T10:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (3,1,'2022-05-10T15:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (3,1,'2022-05-11T10:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (3,1,'2022-05-12T15:30:00');
+
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (6,1,'2022-04-25T15:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (6,1,'2022-05-09T15:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (6,1,'2022-05-09T08:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (6,1,'2022-05-10T10:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (6,1,'2022-05-10T15:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (6,1,'2022-05-11T10:30:00');
+INSERT INTO screenings(movie_movie_type_id, movie_hall_id, start_of_screening) 
+VALUES (6,1,'2022-05-12T15:30:00');
 
